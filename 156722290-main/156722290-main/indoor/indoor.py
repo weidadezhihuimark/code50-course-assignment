ad89fe4c05a13a84343444d652e297c7e6a28cd6
weidadezhihuimark <156722290+weidadezhihuimark@users.noreply.github.com>
2024-01-23 11:00:55 -0700

    Add files via upload

---
+++ 156722290-main/156722290-main/indoor/indoor.py
@@ -0,0 +1,12 @@
+def main():
+
+    user_input = input ("What's your input? ")
+
+    # Convert the input into lowercase
+    lowercase_input = user_input.lower()
+
+    # Output the lowercase
+
+    print(lowercase_input)
+
+main()
