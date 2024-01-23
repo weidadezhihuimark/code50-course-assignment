bfefe83fb7cebfaf3b1e5a46e079db858e11e36e
weidadezhihuimark <156722290+weidadezhihuimark@users.noreply.github.com>
2024-01-23 11:04:20 -0700

    Add files via upload

---
+++ Week 0/playback/playback.py
@@ -0,0 +1,11 @@
+def main():
+    # Prompt the user for input
+    text = input("Enter text: ")
+
+    # Replace spaces with three periods
+    modified_text = text.replace(" ", "...")
+
+    # Print the modified text
+    print(modified_text)
+
+main()
